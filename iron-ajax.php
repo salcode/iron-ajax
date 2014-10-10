039c901de85fc642b4d0bdef4464e87cfdcb5b68
Sal Ferrarello <salcode@salogic.net>
2014-10-10 14:47:02 -0400

    add plugin header

---
+++ iron-ajax.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Plugin Name: Iron Ajax
+ * Plugin URI: http://salferrarello.com/
+ * Description: Sets up an instance of class Fe_Ajx, which creates a WordPress admin page for processing lots of items through ajax calls.
+ * Version: 0.1.0
+ * Author: Sal Ferrarello
+ * Author URI: http://salferrarello.com/
+ * Text Domain: iron-ajax
+ * Domain Path: /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
+include( 'fe-ajx-class.php' );
