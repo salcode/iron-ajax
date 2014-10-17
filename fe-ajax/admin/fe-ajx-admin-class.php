aaeae88a144a1a33c94bd3b6833784cfca31bb1c
Sal Ferrarello <salcode@salogic.net>
2014-10-17 13:56:37 -0400

    refactor enqueueing assets to make debug true happy

--- fe-ajax/admin/fe-ajx-admin-class.php
+++ fe-ajax/admin/fe-ajx-admin-class.php
@@ -8,14 +8,17 @@ class Fe_Ajx_Admin {
 	public function __construct( $instance_id ) {
 		$this->instance_id = $instance_id;
 		$this->instance_slug = Fe_Ajx::slugify( $this->instance_id );
-		self::register_assets();
-		$this->args = apply_filters( "fe_ajx_{$instance_slug}_admin_args", $this->args( $instance_id ) );
+		add_action( 'admin_enqueue_scripts', array( __CLASS__, 'register_assets' ) );
+		$this->args = apply_filters( "fe_ajx_{$this->instance_slug}_admin_args", $this->args( $instance_id ) );
 
 		// in method add_menu, we also hook into the generated
 		// menu_hookname to only enqueue assets on this page
 		add_action( 'admin_menu', array( $this, 'add_menu' ) );
 
-		add_action( 'fe_ajx_' . $this->instance_slug . '_template_after',
+		// add developer information on the template page
+		// this can be removed
+		// remove_action( "fe_ajx_{$this->instance_slug}_template_after", array( 'Fe_Ajx_Admin', 'display_developer_hooks' ) );
+		add_action( "fe_ajx_{$this->instance_slug}_template_after",
 			array( __CLASS__, 'display_developer_hooks' ),
 			10, 2
 		);
@@ -87,15 +90,16 @@ class Fe_Ajx_Admin {
 
 	public function hook_enqueue_assets( $hook_suffix ) {
 		// Unfortunately we can't just enqueue our scripts here - it's too early. So register against the proper action hook to do it
-		add_action( 'admin_enqueue_scripts', array( __CLASS__, 'enqueue_assets' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_assets' ) );
+	}
+
+	public function enqueue_assets( $hook_suffix ) {
+		wp_enqueue_style( 'fe-ajx-jquery-ui-progressbar' );
+		wp_enqueue_script( 'fe-ajx-scripts' );
 		$data = array(
 			'instance_id' => $this->instance_id,
 		);
 		wp_localize_script( 'fe-ajx-scripts', 'feAjx', $data );
-	}
 
-	public static function enqueue_assets( $hook_suffix ) {
-		wp_enqueue_style( 'fe-ajx-jquery-ui-progressbar' );
-		wp_enqueue_script( 'fe-ajx-scripts' );
 	}
 }
