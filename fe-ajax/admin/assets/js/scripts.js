5a686cc7e38d0e957e9cbb391a350d0a20b7bf8f
Sal Ferrarello <salcode@salogic.net>
2014-10-17 13:57:16 -0400

    improve alternating classes in template and expanding developer information

--- fe-ajax/admin/assets/js/scripts.js
+++ fe-ajax/admin/assets/js/scripts.js
@@ -130,6 +130,4 @@ jQuery(document).ready( function($) {
 
 	}
 
-
-
 });
--- fe-ajax/admin/views/developer-information.php
+++ fe-ajax/admin/views/developer-information.php
@@ -1,3 +1,4 @@
+	<?php $alt = 1; ?>
 	<h3>Developer Information</h3>
 	<p>
 		The behavior of this page can be modified with the following filters.
@@ -10,28 +11,43 @@
 			</tr>
 		</thead>
 
-		<tr class="alternate">
+		<tr class="<?php echo ( $alt++%2 ? 'alternate' : '' ); ?>">
+			<td><pre>fe_ajx_<?php echo $instance_slug ?>_process</pre></td>
+			<td>
+				Filter to modify the returned object when an item is processed.  Default is
+<pre>
+array(
+	'success' => true,
+	'index'   => $index,
+	'persist' => array(),
+)
+</pre>
+			</td>
+		</tr>
+
+
+		<tr class="<?php echo ( $alt++%2 ? 'alternate' : '' ); ?>">
 			<td><pre>fe_ajx_<?php echo $instance_slug ?>_entries_count</pre></td>
 			<td>
 				Filter to modify the number of items to be processed. Default is <b>100</b>
 			</td>
 		</tr>
 
-		<tr class="alternate">
+		<tr class="<?php echo ( $alt++%2 ? 'alternate' : '' ); ?>">
 			<td><pre>fe_ajx_<?php echo $instance_slug ?>_batch_size</pre></td>
 			<td>
 				Filter to modify the number of items to be processed. Default is <b>20</b>
 			</td>
 		</tr>
 
-		<tr class="alternate">
+		<tr class="<?php echo ( $alt++%2 ? 'alternate' : '' ); ?>">
 			<td><pre>fe_ajx_<?php echo $instance_slug ?>_index_start</pre></td>
 			<td>
-				Filter to modify the index of where to start processing. Default is <b>0</b>
+				Filter to modify the index of where to start processing. Default is <b>0</b><br>
 			</td>
 		</tr>
 
-		<tr class="alternate">
+		<tr class="<?php echo ( $alt++%2 ? 'alternate' : '' ); ?>">
 			<td><pre>fe_ajx_<?php echo $instance_slug ?>_init</pre></td>
 			<td>
 				Filter to modify the returned object when initialization occurs.  Default is
@@ -46,28 +62,18 @@ array(
 			</td>
 		</tr>
 
-		<tr class="alternate">
-			<td><pre>fe_ajx_<?php echo $instance_slug ?>_process</pre></td>
-			<td>
-				Filter to modify the returned object when an item is processed.  Default is
-<pre>
-array(
-	'success' => true,
-	'index'   => $index,
-	'persist' => array(),
-)
-</pre>
-			</td>
-		</tr>
-
-		<tr class="alternate">
-			<td><pre>fe_ajx_<?php echo $instance_slug ?>_process</pre></td>
-			<td>Filter to modify the number of items to be processed</td>
-		</tr>
-
 		<tr>
 			<td><pre>fe_ajx_<?php echo $instance_slug ?>_template_after</pre></td>
-			<td>Action to display after the content on admin page.</td>
+			<td>
+				Action to display after the content on admin page. This action is being
+				used to display this developer information and can be removed with
+<pre>
+remove_action(
+	'fe_ajx_iron_ajax_template_after',
+	array( 'Fe_Ajx_Admin', 'display_developer_hooks' )
+);
+</pre>
+			</td>
 		</tr>
 
 	</table>
