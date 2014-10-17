8f99481df52ab76389095a6c74a4d7903ad48d76
Sal Ferrarello <salcode@salogic.net>
2014-10-17 15:06:39 -0400

    improve built in documentation

--- fe-ajax/admin/views/developer-information.php
+++ fe-ajax/admin/views/developer-information.php
@@ -30,6 +30,17 @@ array(
 			<td><pre>fe_ajx_<?php echo $instance_slug ?>_entries_count</pre></td>
 			<td>
 				Filter to modify the number of items to be processed. Default is <b>100</b>
+				<h4>Example</h4>
+<pre>
+add_filter(
+	'fe_ajx_<?php echo $instance_slug; ?>_entries_count',
+	'examp_iajx_entries_count'
+);
+function examp_iajx_entries_count( $count ) {
+	return 1000;
+}
+</pre>
+
 			</td>
 		</tr>
 
@@ -62,18 +73,41 @@ array(
 			</td>
 		</tr>
 
-		<tr>
+		<tr class="<?php echo ( $alt++%2 ? 'alternate' : '' ); ?>">
 			<td><pre>fe_ajx_<?php echo $instance_slug ?>_template_after</pre></td>
 			<td>
 				Action to display after the content on admin page. This action is being
 				used to display this developer information and can be removed with
 <pre>
-remove_action(
-	'fe_ajx_iron_ajax_template_after',
-	array( 'Fe_Ajx_Admin', 'display_developer_hooks' )
-);
+add_action( 'init', 'hide_iajx_dev_notes' );
+function hide_iajx_dev_notes() {
+	remove_action(
+		'fe_ajx_<?php echo $instance_slug; ?>_template_after',
+		array(
+			'Fe_Ajx_Admin',
+			'display_developer_hooks'
+		)
+	);
+}
 </pre>
 			</td>
 		</tr>
+
+		<tr class="<?php echo ( $alt++%2 ? 'alternate' : '' ); ?>">
+			<td><pre>fe_ajx_<?php echo $instance_slug ?>_template_heading</pre></td>
+			<td>
+				Filter to modify the heading on the admin page
+<pre>
+add_filter(
+	'fe_ajx_<?php echo $instance_slug; ?>_ajax_template_heading',
+	'examp_iajx_templ_head'
+);
+function examp_iajx_templ_head() {
+	return 'This is a New Heading';
+}
+</pre>
+			</td>
+		</tr>
+
 
 	</table>
